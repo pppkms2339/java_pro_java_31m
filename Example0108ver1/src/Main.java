798ddd3dbe66e077f66c198f6948568b3341878a
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-02 09:37:44 +1000

    Объявление и инициализация массива объектов класса Box. использование массива объектов.

--- Example0108ver1/src/Main.java
+++ Example0108ver1/src/Main.java
@@ -4,15 +4,13 @@ public class Main {
         // Объявить, выделить память и инициализировать объекты класса Box
         Box myBox1 = new Box(10, 20, 15);
         Box myBox2 = new Box(3, 6, 9);
-        double volume;
 
-        // Получить объем первого параллелепипеда
-        volume = myBox1.volume();
-        System.out.println("Volume = " + volume);
-
-        // Получить объем второго параллелепипеда
-        volume = myBox2.volume();
-        System.out.println("Volume = " + volume);
+        // Создание массива объектов класса Box и вывод объема каждого из
+        // параллелепипедов в цикле
+        Box[] boxes = {myBox1, myBox2};
+        for (int i = 0; i < boxes.length; i++) {
+            System.out.println("Volume = " + boxes[i].volume());
+        }
     }
 
 }
