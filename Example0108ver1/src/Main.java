2e0245ce18d57e2885098bcdb5af4c7dc9890fa0
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-02 09:18:12 +1000

    Объявление двух объектов класса Box

--- Example0108ver1/src/Main.java
+++ Example0108ver1/src/Main.java
@@ -1,17 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        Box myBox = new Box();  // создать объект myBox класса Box
+        Box myBox1 = new Box();  // создать первый объект myBox класса Box
+        Box myBox2 = new Box();  // создать второй объект myBox класса Box
         double volume;
 
-        // Присвоить значения переменным экземпляра (полям) myBox
-        myBox.width = 10;
-        myBox.height = 20;
-        myBox.depth = 15;
+        // Присвоить значения переменным экземпляра (полям) myBox1
+        myBox1.width = 10;
+        myBox1.height = 20;
+        myBox1.depth = 15;
 
-        // Рассчитать объем параллелепипеда
-        volume = myBox.width * myBox.height * myBox.depth;
+        // Присвоить значения переменным экземпляра (полям) myBox2
+        myBox2.width = 3;
+        myBox2.height = 6;
+        myBox2.depth = 9;
 
+        // Рассчитать объем первого параллелепипеда
+        volume = myBox1.width * myBox1.height * myBox1.depth;
+        System.out.println("Volume = " + volume);
+
+        // Рассчитать объем второго параллелепипеда
+        volume = myBox2.width * myBox2.height * myBox2.depth;
         System.out.println("Volume = " + volume);
     }
 
