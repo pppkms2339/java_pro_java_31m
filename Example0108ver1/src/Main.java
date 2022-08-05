942f5d079973d038067147b063c5f238520cb2a6
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-05 20:48:01 +1000

    переменное число параметров

--- Example0108ver1/src/Main.java
+++ Example0108ver1/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        outer.test();
+        test(10);
+        System.out.println();
+        test(1, 2, 3);
+        System.out.println();
+        test();
+        System.out.println(Arrays.toString(args));
+    }
+
+    private static void test(int... v) {
+        System.out.println("length = " + v.length);
+        for (int p : v) {
+            System.out.println(p);
+        }
     }
 
 }
