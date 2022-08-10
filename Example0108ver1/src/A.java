8c62b10330dede4886e745d8524b80af5d9df0c2
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-10 20:12:09 +1000

    Принцип, что ссылочная переменная из суперкласса может ссылаться на объект подкласса

--- Example0108ver1/src/A.java
+++ Example0108ver1/src/A.java
@@ -1,26 +1,5 @@
 public class A {
-    int i, j;
 
-    public A(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
+    int i;
 
-    // Перегруженный конструктор
-    public A() {
-        this.i = 0;
-        this.j = 0;
-    }
-
-    void show() {
-        System.out.println("i = " + i + ", j = " + j);
-    }
-
-    @Override
-    public String toString() {
-        return "A{" +
-                "i=" + i +
-                ", j=" + j +
-                '}';
-    }
 }
--- Example0108ver1/src/B.java
+++ Example0108ver1/src/B.java
@@ -1,25 +1,5 @@
 public class B extends A {
 
-    int k;
+    int j;
 
-    public B(int i, int j, int k) {
-        super(i, j);
-        this.k = k;
-    }
-
-    // Вывести содержимое переменной k с помощью метода,
-    // переопределяющего метод show() из класса А
-    @Override
-    void show() {
-        super.show();   // Здесь вызывается метод show() из класса А
-        System.out.println("k = " + k);
-    }
-
-    @Override
-    public String toString() {
-        String superString = super.toString();
-        return "B{" +
-                "k=" + k +
-                "} " + superString;
-    }
 }
