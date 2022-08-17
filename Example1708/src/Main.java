9b99b8ff8a307f36ff633ca515bf8137beef7921
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-17 20:26:51 +1000

    Обобщенный метод

--- Example1708/src/Main.java
+++ Example1708/src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        TwoGen<Integer, String> a = new TwoGen<>(12, "qwerty");
-        a.showTypes();
+        String[] array1 = {"qwerty", "asd"};
+        Integer[] array2 = {1, 2, 3, 4, 5};
+        print(array1);
+        print(array2);
+    }
+
+    public static <T> void print(T[] items) {
+        for(T item : items) {
+            System.out.println(item);
+        }
     }
 
 }
