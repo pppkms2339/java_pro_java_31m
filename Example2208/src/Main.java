5ed099f17f90fe64c81af90eca4964a1a3e12e35
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-22 17:42:26 +1000

    Решение домашнего задания Level1

---
+++ Example2208/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(12);
+        Stack<Employee> stack1 = new Stack<>();
+        stack1.push(new Employee());
+    }
+
+}
