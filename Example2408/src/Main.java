6ef1ebfd3c60fb944057bbf45590f72a21026ddf
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-24 19:39:13 +1000

    Удаление из массивов

--- Example2408/src/Main.java
+++ Example2408/src/Main.java
@@ -1,92 +1,24 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<>();
-        a.add(3);
-        a.add(4);
-        a.add(2);
-        ArrayList<Integer> b = new ArrayList<>();
-        b.add(7);
-        b.add(7);
-        b.add(4);
-
-//        int number = getNumber(a);
-//        System.out.println(number);
-//        System.out.println(getArrayList(number));
-//        System.out.println(getNumber2(a));
-
-        // Решение
-        int number1 = getNumber(a);
-        int number2 = getNumber2(b);
-        int answer = number1 + number2;
-        System.out.println(getArrayList(answer));
-
-        // Решение с помощью улучшенного алгоритма
-        System.out.println(sum(a, b));
+        Cat[] cats = {new Cat("Barsik"),
+                new Cat("Murzik"),
+                new Cat("Qwerty"),
+                new Cat("Asdf")};
+        System.out.println(Arrays.toString(cats));
+        cats[1] = null;
+        System.out.println(Arrays.toString(cats));
+        for (int i = 2; i < cats.length; i++) {
+            cats[i - 1] = cats[i];
+            cats[i] = null;
+        }
+        System.out.println(Arrays.toString(cats));
     }
 
-    private static ArrayList<Integer> sum(ArrayList<Integer> a, ArrayList<Integer> b) {
-        ArrayList<Integer> result = new ArrayList<>();
-        int remainder = 0;
-        int i = a.size() - 1, j = b.size() - 1;
-        while (i >= 0 || j >= 0) {
-            int value1 = 0;
-            if (i >= 0) {
-                value1 = a.get(i);
-                i--;
-            }
-            int value2 = 0;
-            if (j >= 0) {
-                value2 = b.get(j);
-                j--;
-            }
-            result.add(0, (value1 + value2 + remainder) % 10);
-            remainder = (value1 + value2 + remainder) / 10;
-        }
-        if (remainder > 0) {
-            result.add(0, remainder);
-        }
-        return result;
+    private void deleteCat(Cat[] cats, int deleteIndex) {
+        
     }
-
-    // Реализуем алгоритм получения числа по разрядам, хранящимся
-    // в ArrayList.
-    private static int getNumber(ArrayList<Integer> a) {
-        String str = "";
-        for (int i = 0; i < a.size(); i++) {
-            str = str + a.get(i);
-        }
-        return Integer.parseInt(str);
-    }
-
-    private static int getNumber2(ArrayList<Integer> a) {
-        int answer = 0;
-        for (int i = a.size() - 1; i >= 0; i--) {
-            answer = answer + a.get(i) * (int) Math.pow(10, a.size() - i - 1);
-        }
-        return answer;
-    }
-
-    private static ArrayList<Integer> getArrayList(int n) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        String str = String.valueOf(n);
-        for (int i = 0; i < str.length(); i++) {
-            // '7' -> "7" -> 7
-            answer.add(Integer.parseInt("" + str.charAt(i)));
-        }
-        return answer;
-    }
-
-    private static ArrayList<Integer> getArrayList2(int n) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        while (n > 0) {
-            answer.add(0, n % 10);
-            n = n / 10;
-        }
-        return answer;
-    }
-
-
 }
