e651b092407b5d95565a0d79f3d76f58a7aecd7e
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-29 20:55:52 +1000

    LinkedList vs ArrayList

--- Example2908/src/Main.java
+++ Example2908/src/Main.java
@@ -1,18 +1,28 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
-        list.add(20);   // добавление в конец списка
-        list.addFirst(10);
-        list.addLast(30);
-        list.add(1, 15);    // добавление в указанный индекс
-        for (Integer i : list) {
-            System.out.println(i);
+        for (int i = 0; i < 5000000; i++) {
+            list.add(Integer.valueOf(i));
         }
-        list.remove();  // удаление с конца
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            list.add(2000000, Integer.valueOf(i));
+        }
+        System.out.println("Time LinkedList: " + (System.currentTimeMillis() - start));
 
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < 5000000; i++) {
+            list2.add(Integer.valueOf(i));
+        }
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            list2.add(2000000, Integer.valueOf(i));
+        }
+        System.out.println("Time ArrayList: " + (System.currentTimeMillis() - start));
     }
 
 }
