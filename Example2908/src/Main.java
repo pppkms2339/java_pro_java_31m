3cbba04338445de8164840903165c9a85e4a4c2e
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-29 20:21:17 +1000

    LinkedList

--- Example2908/src/Main.java
+++ Example2908/src/Main.java
@@ -1,55 +1,18 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Iterator<Integer> integerIterator = list.iterator();
-
-        Integer[] integerArray = {10, 20, 3, -4, -5};
-        ArrayIterator<Integer> arrayIterator = new ArrayIterator<>(integerArray);
-        while (arrayIterator.hasNext()) {
-            int value = arrayIterator.next();
-            if (value == 3) {
-                arrayIterator.remove();
-            }
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(20);   // добавление в конец списка
+        list.addFirst(10);
+        list.addLast(30);
+        list.add(1, 15);    // добавление в указанный индекс
+        for (Integer i : list) {
+            System.out.println(i);
         }
-        System.out.println(Arrays.toString(integerArray));
+        list.remove();  // удаление с конца
 
-        // Создание итератора для строкового массива
-        ArrayIterator<String> arrayIterator2 = new ArrayIterator<>(new String[]{"qwerty", "asdf"});
-
-        // Тестирование второго способа
-        Iterator<Integer> arrayIterator3 = getIterator(integerArray);
-        while (arrayIterator3.hasNext()) {
-            //...
-        }
-    }
-
-    // Реализация с помощью обобщенного метода, возвращающего Iterator
-    private static <T> Iterator<T> getIterator(T[] array) {
-        return new Iterator<T>() {
-
-            private int count = array.length;;
-            private int index = 0;
-            @Override
-            public boolean hasNext() {
-                return index < count;
-            }
-
-            @Override
-            public T next() {
-                if (index < count) {
-                    return array[index++];
-
-                } else {
-                    System.out.println("No such element!");
-                    return null;
-                }
-            }
-        };
     }
 
 }
