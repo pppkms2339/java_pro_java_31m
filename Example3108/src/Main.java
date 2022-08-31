c997f8ec752e3e7135fdc24d0b8899bd088dba9c
Anton Loshmanov <pppkms2339@gmail.com>
2022-08-31 19:31:24 +1000

    Попытка решения задачи № 2

--- Example3108/src/Main.java
+++ Example3108/src/Main.java
@@ -1,47 +1,30 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list1 = new LinkedList<>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(4);
-        List<Integer> list2 = new LinkedList<>();
-        list2.add(1);
-        list2.add(3);
-        list2.add(4);
-        list2.add(6);
-        list2.add(9);
-        List<Integer> list3 = new LinkedList<>();
-        int i = 0, j = 0;
-        while (i < list1.size() && j < list2.size()) {
-            if (list1.get(i) < list2.get(j)) {
-                list3.add(list1.get(i));
-                i++;
-            } else {
-                list3.add(list2.get(j));
-                j++;
-            }
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(rotateList(list, 2));
+    }
+
+    private static List<Integer> rotateList(List<Integer> list, int k) {
+        if (k == 0) {
+            return list;
         }
-        if (i < list1.size()) {
-            // В первом списке остались элементы, копируем их
-            list3.addAll(list1.subList(i, list1.size()));
-//            while (i < list1.size()) {
-//                list3.add(list1.get(i));
-//                i++;
-//            }
+        int length = list.size();
+        List<Integer> result = new ArrayList<>(length);
+        for (int i = 0; i < length; i++) {
+            result.add(k % length, list.get(i));
+            k++;
         }
-        if (j < list2.size()) {
-            // Во втором списке остались элементы, копируем их
-            list3.addAll(list2.subList(j, list2.size()));
-//            while (j < list2.size()) {
-//                list3.add(list2.get(j));
-//                j++;
-//            }
-        }
-        System.out.println(list3);
+        return result;
     }
 
 }
