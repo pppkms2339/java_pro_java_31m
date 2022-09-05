9a95e4c83cfc6f961741acf969f4161f62c33d57
Anton Loshmanov <pppkms2339@gmail.com>
2022-09-05 18:49:04 +1000

    Решение второй задачи из ДЗ

--- Example0509/src/Main.java
+++ Example0509/src/Main.java
@@ -6,34 +6,21 @@ public class Main {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
+        list.add(2);
         list.add(3);
         list.add(4);
-        list.add(5);
-        list.add(6);
-//        shift1(list, 7);
-//        System.out.println(list);
-        System.out.println(shift2(list, 7));
-    }
-
-    private static void shift1(ArrayList<Integer> list, int k) {
-        k = k % list.size();
-        for (int i = 0; i < k; i++) {
-            int last = list.get(list.size() - 1);
-            list.add(0, last);
-            list.remove(list.size() - 1);
+        list.add(4);
+        list.add(4);
+        int count = 1;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) != list.get(i + 1)) {
+                list.set(count, list.get(i + 1));
+                count++;
+            }
         }
-    }
-
-    private static ArrayList<Integer> shift2(ArrayList<Integer> list, int k) {
-        ArrayList<Integer> result = new ArrayList<>();
-        int j = -k;
-        while (j < 0) {
-            j += list.size();
-        }
-        for (int i = 0; i < list.size(); i++) {
-            result.add(list.get((i + j) % list.size()));
-        }
-        return result;
+        System.out.println(list);
+        System.out.println(count);
+        System.out.println(list.subList(0, count));
     }
 
 }
